6d781ef3271a0242ad48cf2458e1a1b2c03155a9
Buddy Favors Jr <buddyfavors@gmail.com>
2017-11-30 10:15:25 -0500

    Saving progress.

--- BathroomStatus.Sensor/MainPage.xaml.cs
+++ BathroomStatus.Sensor/MainPage.xaml.cs
@@ -1,30 +1,90 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+using Windows.UI;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace BathroomStatus.Sensor
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
+        private const int DOOR_PIN = 5;
+
+        private GpioController m_Gpio = null;
+        private GpioPin m_DoorPin = null;
+        private bool m_IsOpened = true;
+        private readonly DispatcherTimer m_Timer = null;
+
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+
+            InitGPIOAsync();
+
+            Unloaded += MainPage_Unloaded;
+
+            m_Timer = new DispatcherTimer()
+            {
+                Interval = TimeSpan.FromMilliseconds(100)
+            };
+            m_Timer.Tick += Timer_Tick;
+            //m_Timer.Start();
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            m_DoorPin.Write(!IsOpened() ? GpioPinValue.Low : GpioPinValue.High);
+
+            UpdateStatusAsync();
+        }
+
+        private void MainPage_Unloaded(object sender, RoutedEventArgs e)
+        {
+            m_DoorPin.Dispose();
+        }
+
+        private async void InitGPIOAsync()
+        {
+            m_Gpio = await GpioController.GetDefaultAsync();
+            
+            m_DoorPin = m_Gpio.OpenPin(DOOR_PIN);
+            m_DoorPin.DebounceTimeout = TimeSpan.FromMilliseconds(50);
+            //m_DoorPin.Write(GpioPinValue.High);
+            m_DoorPin.SetDriveMode(GpioPinDriveMode.Input);
+            m_DoorPin.ValueChanged += DoorPin_ValueChanged;
+        }
+
+        private void DoorPin_ValueChanged(GpioPin sender, GpioPinValueChangedEventArgs args)
+        {
+            m_IsOpened = !m_IsOpened;
+            UpdateStatusAsync();
+        }
+
+        private bool IsOpened()
+        {
+            return m_IsOpened;
+            var value = m_DoorPin.Read();
+            return value == GpioPinValue.Low;
+        }
+
+        private async void UpdateStatusAsync()
+        {
+            var isOpened = IsOpened();
+
+            await RunOnUIThreadAsync(CoreDispatcherPriority.High, () =>
+            {
+                var brush = new SolidColorBrush(isOpened ? Color.FromArgb(255, 0, 255, 0) : Color.FromArgb(255, 255, 0, 0));
+                m_Overlay.Background = brush;
+            });
+        }
+
+        private async Task RunOnUIThreadAsync(CoreDispatcherPriority priority, Action action)
+        {
+            await Dispatcher.RunAsync(priority, () => action());
         }
     }
 }
